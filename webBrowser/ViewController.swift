b87731a2b718ef46c8de087fada094d062cc490e
Sol <solbattaglia@gmail.com>
2023-02-09 15:52:41 -0300

    Added webket to render a web browser

--- webBrowser/ViewController.swift
+++ webBrowser/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate{
+    var webView: WKWebView!
+        
+        override func loadView() {
+            let webConfiguration = WKWebViewConfiguration()
+            webView = WKWebView()
+            webView.navigationDelegate = self
+            view = webView
+        }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let myURL = URL(string:"https://www.apple.com")
+               let myRequest = URLRequest(url: myURL!)
+               webView.load(myRequest)
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
